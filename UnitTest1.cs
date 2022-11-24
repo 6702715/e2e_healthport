231c590057dc05bacc61734514ed277483c2089a
puzanov <6702715@gmail.com>
2022-11-24 18:48:05 +0000

    11111

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -19,30 +19,29 @@ public class Tests
     public void Setup()
     {
         this.driver = new EdgeDriver();
-        this.driver.Url = "http://192.168.56.3:8088/"; //set my url
+        this.driver.Url = "http://192.168.56.3:5044/about"; //set my url
     }
 
     [Test]
-    public void Login()
+    public void AddFeedBack()
     {
-        this.driver.FindElement(By.XPath("//a[@class='ico-login']")).Click();
-        Thread.Sleep(3000);
+        IWebElement? tbEmail = this.driver?.FindElement(By.Id("email"));  
+        tbEmail?.Clear();
+        tbEmail.SendKeys("user@gmail.com");
 
-        IWebElement loginField = this.driver.FindElement(By.XPath("//input[@id='Email']")); 
-        loginField.Clear();
-        loginField.SendKeys("puzanov@localnet.ua");
+        IWebElement? tbMessage = this.driver?.FindElement(By.Id("textArea"));  
+        tbMessage.Clear();
+        tbMessage.SendKeys("AAA bbb CCC");
 
-        IWebElement passwordField = this.driver.FindElement(By.XPath("//input[@id='Password']"));  
-        passwordField.Clear();
-        passwordField.SendKeys("Pa$$w0rd");
 
-        this.driver.FindElement(By.XPath("//button[normalize-space()='Log in']")).Click();
-        IWebElement logoutLink = this.driver.FindElement(By.XPath("//a[@class='ico-logout']"));
-        System.Console.WriteLine(logoutLink.Text);
-        //Assert.That(logoutLink.Text == "LOG OUT", "You are not logged!");
-        Code.BugIf(logoutLink.Text == "LOG OUT","You are not logged!");
+        //IWebElement? submitButton = this.driver?.FindElement(By.XPath("//button[@type='submit']"));
+        IWebElement? btnSubmit = this.driver?.FindElement(By.Id("submit"));
+        btnSubmit.Click();
+        Thread.Sleep(1000);
 
-        Thread.Sleep(3000);
+        IWebElement? textElement = this.driver.FindElement(By.XPath("//h2"));
+        string submitMessageText = textElement.Text.ToUpper();
+        Code.AssertState(string.Equals(submitMessageText, "Thank you, your message has been sent and will be reviewed shortly".ToUpper()), "submitMessage is not valid!");
     }
 
     [TearDown]
